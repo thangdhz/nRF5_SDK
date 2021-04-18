c8d253e6ca11104a9bf50595da2d4d9c898e52c7
ThangDH <thangdh92@gmail.com>
2021-04-18 23:35:40 +0700

    add board_config, led and update storage

--- components/boards/pca10040.h
+++ components/boards/pca10040.h
@@ -143,8 +143,8 @@ extern "C" {
 
 
 // Arduino board mappings
-#define ARDUINO_SCL_PIN             27    // SCL signal pin
-#define ARDUINO_SDA_PIN             26    // SDA signal pin
+#define ARDUINO_SCL_PIN             12    // SCL signal pin
+#define ARDUINO_SDA_PIN             4    // SDA signal pin
 #define ARDUINO_AREF_PIN            2     // Aref pin
 #define ARDUINO_13_PIN              25    // Digital pin 13
 #define ARDUINO_12_PIN              24    // Digital pin 12
